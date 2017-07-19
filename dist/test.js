4d8fb924bbcb797905bc015ecbbe3101803302d5
Jasper De Moor <jdemo@live.be>
2017-07-19 14:41:34 +0200

    improve test example to include change time

--- dist/test.js
+++ dist/test.js
@@ -5,6 +5,31 @@ const planner = new Client(entryPoints);
 const now = new Date();
 const inAnHour = new Date(now.valueOf() + 60 * 60 * 1000);
 
+countChanges = (resultSet) => {
+    let tripCount = 0;
+    let lastTrip = resultSet[0]['http://vocab.gtfs.org/terms#trip'];
+    for (let connection of resultSet) {
+        let currTrip = connection['http://vocab.gtfs.org/terms#trip'];
+        if (lastTrip !== currTrip) {
+            tripCount++;
+        }
+        lastTrip = currTrip;
+    }
+    return tripCount;
+};
+
+timeBetweenChanges = (resultSet) => {
+    let times = [];
+    let lastConn = resultSet[0];
+    for (let connection of resultSet) {
+        if (lastConn['http://vocab.gtfs.org/terms#trip'] !== connection['http://vocab.gtfs.org/terms#trip']) {
+            times.push((connection.departureTime - lastConn.arrivalTime) / 60000);
+        }
+        lastConn = connection;
+    }
+    return times;
+};
+
 runQuery = () => {
     planner.query({
         departureStop: "http://irail.be/stations/NMBS/008892007",
@@ -34,11 +59,12 @@ runQuery = () => {
             console.log("Total connections processed: ", dataCount);
             console.log("Total requests send: ", requestCount);
             console.log("Total responses gotten: ", responseCount);
+            console.log("Total changes: ", countChanges(path));
+            console.log("Times between changes: ", timeBetweenChanges(path), " minutes");
         });
     });
 };
 
 (() => {
-    console.log("RUN IIFE");
     runQuery();
 })()
